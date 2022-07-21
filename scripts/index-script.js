f0f68c286ffca561e50bfc01fe57116c704a64d1
Ryan Staudacher <ryanmstaudacher@gmail.com>
2022-07-21 17:34:56 -0500

    Changed SVG's to be cutouts instead of absolutely positioned images.

--- scripts/index-script.js
+++ scripts/index-script.js
@@ -1,22 +1,32 @@
 const modeButton = document.getElementById('btn');
 const svg1 = document.getElementById("headerSVG1");
 const svg2 = document.getElementById('headerSVG2');
-const h1 = document.getElementsByTagName('h1');
+/*const h1 = document.getElementsByTagName('h1');*/
+
+var elements = document.getElementsByTagName('*');
 
 modeButton.addEventListener('click', function onClick(event){
      if(svg1.style.visibility === 'visible')
      {
           svg1.style.visibility = 'hidden';
           svg2.style.visibility = 'visible';
-          document.body.style.color = '#001220'
-          document.body.style.backgroundColor = '#EAEBED';
+
+          for(var i=0; i < elements.length; i++)
+          {
+               elements[i].style.color = '#001220';
+               elements[i].style.backgroundColor = '#EAEBED';
+          }
      }
      else if(svg1.style.visibility === 'hidden')
      {
           svg2.style.visibility = 'hidden';
           svg1.style.visibility = 'visible';
-          document.body.style.color = '#EAEBED'
-          document.body.style.backgroundColor = '#001220';
+
+          for(var i=0; i < elements.length; i++)
+          {
+               elements[i].style.color = '#EAEBED';
+               elements[i].style.backgroundColor = '#001220';
+          }
      }
 });
 
