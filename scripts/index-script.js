66f4f3a183c0b98962347b209ea2f19b9516c356
Ryan Staudacher <ryanmstaudacher@gmail.com>
2022-09-13 00:38:21 -0500

    Added links to projects on each project page. Added link to github page. Added fix for elements that were resetting background opacity on theme switch. Added style to Switch Theme button to fit in with the rest of the site.

--- scripts/index-script.js
+++ scripts/index-script.js
@@ -3,30 +3,56 @@ const svg1 = document.getElementById("headerSVG1");
 const svg2 = document.getElementById('headerSVG2');
 /*const h1 = document.getElementsByTagName('h1');*/
 
+const gitlogow = document.getElementById("githublogowhite");
+const gitlogob = document.getElementById("githublogoblack");
+
+const myback = document.getElementById('mybackground');
+const wiplink = document.getElementById('wiplink');
+
 var elements = document.getElementsByTagName('*');
 
 modeButton.addEventListener('click', function onClick(event){
      if(svg1.style.visibility === 'visible')
      {
-          svg1.style.visibility = 'hidden';
-          svg2.style.visibility = 'visible';
+         svg1.style.visibility = 'hidden';
+         svg2.style.visibility = 'visible';
 
-          for(var i=0; i < elements.length; i++)
-          {
-               elements[i].style.color = '#001220';
-               elements[i].style.backgroundColor = '#EAEBED';
-          }
+         gitlogow.style.visibility = 'hidden';
+         gitlogob.style.visibility = 'visible';
+
+         for(var i=0; i < elements.length; i++)
+         {
+             elements[i].style.color = '#001220';
+             elements[i].style.backgroundColor = '#EAEBED';
+         }
+
+         modeButton.style.border = '1px solid #001220';
+         modeButton.parentElement.style.background = '0,0,0,0';
+
+         myback.style.borderLeft = '1px solid #001220';
+
+         wiplink.style.background = '0,0,0,0';
      }
      else if(svg1.style.visibility === 'hidden')
      {
-          svg2.style.visibility = 'hidden';
-          svg1.style.visibility = 'visible';
+         svg2.style.visibility = 'hidden';
+         svg1.style.visibility = 'visible';
 
-          for(var i=0; i < elements.length; i++)
-          {
-               elements[i].style.color = '#EAEBED';
-               elements[i].style.backgroundColor = '#001220';
-          }
+         gitlogow.style.visibility = 'visible';
+         gitlogob.style.visibility = 'hidden';
+
+         for(var i=0; i < elements.length; i++)
+         {
+             elements[i].style.color = '#EAEBED';
+             elements[i].style.backgroundColor = '#001220';
+         }
+
+         modeButton.style.border = '1px solid #EAEBED';
+         modeButton.parentElement.style.background = '0,0,0,0';
+
+         myback.style.borderLeft = '1px solid #EAEBED';
+
+         wiplink.style.background = '0,0,0,0';
      }
 });
 
